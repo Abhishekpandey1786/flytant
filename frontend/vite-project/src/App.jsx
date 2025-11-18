61d4ac4823318f1f0c0ecf99a3e922933e058af8
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2025-11-18 20:46:30 +0530

    Added new

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -47,7 +47,6 @@ function App() {
               <Route path="/blogs" element={<Blog />} />
               <Route path="/privacy-policy" element={<PrivacyPolicy />} />
               <Route path="/terms-of-service" element={<TermsOfService />} />
-               <Route path="/payment-success" element={<PaymentSuccess />} />
             <Route element={<PrivateRoutes />}>
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/dashboard/brand" element={<Brand />} />
@@ -67,7 +66,7 @@ function App() {
               <Route path="/SubscriptionPlans" element={<SubscriptionPlans />}/>
               <Route path="/UserNotifications" element={<UserNotifications />}/>
               <Route path="/my-orders" element={<MyOrders />} />
-             
+              <Route path="/payment-success" element={<PaymentSuccess />} />
               <Route path="/chats" element={<Chats />} />
             </Route>
           </Routes>
