99d52c18b518b2a1e265f014ea5f7c3eaf487eda
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2025-11-25 17:31:28 +0530

    Added new

--- frontend/vite-project/src/assets/SubscriptionPlans.jsx
+++ frontend/vite-project/src/assets/SubscriptionPlans.jsx
@@ -13,7 +13,7 @@ import p7 from "./image/p7.webp";
 import p8 from "./image/p8.webp";
 
 const plans = [
-    { name: "Basic", title: "Billed Monthly", price: 1, oldPrice: 4, discount: "Get 20% Off" },
+    { name: "Basic", title: "Billed Monthly", price: 2, oldPrice: 4, discount: "Get 20% Off" },
     { name: "Standard", title: "Billed Monthly", price: 5, oldPrice: 7, discount: "Get 30% Off" },
     { name: "Advance", title: "Billed Monthly", price: 9, oldPrice: 18, discount: "Get 40% Off" },
     { name: "Premium", title: "Billed Monthly", price: 1900, oldPrice: 39, discount: "Get 50% Off" },
