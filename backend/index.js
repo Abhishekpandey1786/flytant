5556523d7ddd1e4f0e90a0b5314c9e75ac233e69
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2025-12-03 10:55:34 +0530

    Added webhook.

--- backend/index.js
+++ backend/index.js
@@ -41,8 +41,8 @@ connectDB();
 
 app.use(cors());
 app.use("/api/cashfree/webhook",
-    express.raw({ type: 'application/json' })
-);
+    express.raw({ type: '*/*' }) 
+)
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
