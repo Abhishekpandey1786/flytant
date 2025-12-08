e6c020cecbe249b716c60a429f92217fc5788e37
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2025-12-08 17:51:22 +0530

    x

--- backend/routes/cashfreeRoutes.js
+++ backend/routes/cashfreeRoutes.js
@@ -121,8 +121,8 @@ router.post("/webhook", async (req, res) => {
 
     try {
         
-        const signature = req.headers["x-webhook-signature"];
-        const timestamp = req.headers["x-webhook-timestamp"];
+        const signature = req.headers["x-cf-signature-v5"];
+        const timestamp = req.headers["x-cf-timestamp"];
         
         
         if (!signature || !timestamp) {
