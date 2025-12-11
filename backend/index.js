51d456b4c150ed3e558a96117013c35b90491afd
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2025-12-11 18:56:23 +0530

    cashfree

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ connectDB();
 app.use(cors());
 
 // app.use("/api/cashfree/webhook", express.raw({ type: "application/json" }));
-app.use("/api/cashfree", cashfreeRoutes);
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
@@ -54,7 +54,7 @@ app.use("/api/news", newsRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api", publicRoutes);
 app.use("/api/contact", contactRoutes);
-
+app.use("/api/cashfree", cashfreeRoutes);
 
 const connectedUsers = new Map();
 
