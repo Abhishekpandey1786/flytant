15ea4f397550646efc038729dc0351e6fe05010a
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2025-12-15 18:25:16 +0530

    profile

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -5,4 +5,24 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    
+    exclude: [
+        'react-icons/ti',
+        'react-icons/fa',
+        'react-icons/md',
+        'react-icons/bi',
+        'react-icons/fi',
+        'react-icons/io5',
+        'browser-image-compression',
+        'socket.io-client',
+        'lucide-react',
+    ],
+  },
 })
+
+
+
+
+
+  
