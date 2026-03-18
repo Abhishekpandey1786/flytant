fdfa4f176a75129ac18e7ffca075b6574730a7e4
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2026-03-18 19:25:25 +0530

    strip.js

--- backend/index.js
+++ backend/index.js
@@ -35,8 +35,11 @@ const io = new Server(server, {
 connectDB();
 app.use(cors());
 
-app.use("/api/stripe", express.raw({ type: "application/json" }));
-
+app.post(
+  "/api/stripe/webhook",
+  express.raw({ type: "application/json" }),
+  stripeRoutes,
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
