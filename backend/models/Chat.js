560ec466fdc8e56e75a0428dd099ade223374b99
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2026-04-29 10:03:30 +0530

    chats

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -1,10 +1,28 @@
 const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema({
-  roomId: { type: String, required: true },
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  text: { type: String, required: true },
-  senderName: { type: String }, // <-- Ye add karein
-  createdAt: { type: Date, default: Date.now },
+  roomId: {
+    type: String,
+    required: true,
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", 
+    required: true,
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", 
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+module.exports = mongoose.model("Chat", chatSchema);
