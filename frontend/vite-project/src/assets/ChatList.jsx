2d84cdd4587f1f35ad159d8ce8a2ae09177b8a03
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2026-07-17 18:21:48 +0530

    campaign

--- frontend/vite-project/src/assets/ChatList.jsx
+++ frontend/vite-project/src/assets/ChatList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import socket from "../socket";
+import socket from "./socket";
 import { AuthContext } from "./AuthContext.jsx";
 
 const api = axios.create({
