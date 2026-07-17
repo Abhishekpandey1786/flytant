92a28e8c76074b9e7159c9b7c35b1651327e8fbd
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2026-07-17 17:13:19 +0530

    campaign

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
-const chatSchema = new mongoose.Schema({
-  roomId: { type: String, required: true },
-  campaignId: { type: mongoose.Schema.Types.ObjectId, ref: "Campaign" }, // 👈 YE LINE ADD KARO
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  text: String,
-  isRead: { type: Boolean, default: false },
-}, { timestamps: true });
+
+const chatSchema = new mongoose.Schema(
+  {
+    roomId: { type: String, required: true },
+    campaignId: { type: mongoose.Schema.Types.ObjectId, ref: "Campaign" },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    senderName: String,
+    senderAvatar: String,
+    text: String,
+    isRead: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Chat", chatSchema);
