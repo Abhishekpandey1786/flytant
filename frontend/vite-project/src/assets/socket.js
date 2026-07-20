4df3ab7c6fd28df4e4887c955d7a8ccad6ef9bed
Abhishek Pandey <your_abhishekpandey3119@gmail.com>
2026-07-20 16:43:46 +0530

    socket

--- frontend/vite-project/src/assets/socket.js
+++ frontend/vite-project/src/assets/socket.js
@@ -1,16 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io(
-  "https://vistafluence.onrender.com",
-  {
-    transports: ["websocket"],
-
-    reconnection: true,
-
-    reconnectionAttempts: 5,
-
-    reconnectionDelay: 1000,
-  }
-);
+const socket = io("https://vistafluence.onrender.com", {
+  transports: ["websocket", "polling"],
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+});
 
 export default socket;
